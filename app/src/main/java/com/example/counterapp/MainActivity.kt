1e8cd32e51c141e70c504f624e659f87b25b4a37
Aysu <aysuosmanova84@gmail.com>
2026-03-12 00:16:29 +0400

    Started to implement a new counter app project using viewmodel

---
+++ app/src/main/java/com/example/counterapp/MainActivity.kt
@@ -0,0 +1,75 @@
+package com.example.counterapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.counterapp.ui.theme.CounterAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CounterAppTheme {
+                TheCounterApp()
+            }
+        }
+    }
+}
+
+@Composable
+fun TheCounterApp(){
+    val count = remember { mutableStateOf(0 ) }
+    fun increment(){
+        count.value++
+    }
+    fun decrement(){
+        count.value--
+    }
+    Scaffold ( modifier = Modifier.fillMaxSize())
+    { innerPadding ->
+       Column (modifier = Modifier
+           .fillMaxSize()
+           .padding(innerPadding),
+           verticalArrangement = Arrangement.Center,
+           horizontalAlignment = Alignment.CenterHorizontally
+       ){
+           Text(
+               "Count ${count.value } ",
+               fontSize = 24.sp,
+               fontWeight = FontWeight.Bold
+
+           )
+           Spacer(modifier = Modifier.height(16.dp))
+
+           Row() {
+               Button(onClick = {increment()}) {
+                   Text("Increment")
+               }
+               Button(onClick = {decrement()}) {
+                   Text("Decrement")
+               }
+           }
+       }
+    }
+}
